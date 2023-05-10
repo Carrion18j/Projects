6620b7e0dfce292aa6a36e2936c9748d19d1bcc6
Carrion <Carrion18j@gmail.com>
2023-05-10 23:51:22 +0530

    .

--- portfolio-advanced-calculater/src/sections/calculater/index.jsx
+++ portfolio-advanced-calculater/src/sections/calculater/index.jsx
@@ -4,12 +4,46 @@ import "./style/style.css";
 
 const Calculater = () => {
   const [errer, setErrer] = useState(false);
-  const [value, setValue] = useState(0);
-  
+  const [value, setValue] = useState("");
+
   if (localStorage.getItem("lastValue") === "undefined") {
     localStorage.setItem("lastValue", 0);
   }
-  
+
+  const result = (propValue) => {
+    switch (propValue) {
+      case propValue:
+        if (propValue === "x") {
+          setValue(value + "*");
+          break;
+        }
+      case propValue:
+        if (propValue === "C") {
+          setValue("");
+          break;
+        }
+      case propValue:
+        if (propValue === "=") {
+          try {
+            setErrer(false);
+            const Value = (eval(value)).toString().substring(0, 8)
+            console.log(Value)
+            localStorage.setItem("lastValue", Value);
+            setValue(Value);
+          } 
+          catch (errer) {
+            console.log(value)
+            setErrer(true);
+            setValue("");
+          }
+          break;
+        }
+      case propValue:
+        setValue(value + `${propValue}`);
+        break;
+    }
+  };
+
   return (
     <section>
       <div className="calculater_container">
@@ -21,7 +55,7 @@ const Calculater = () => {
           <input placeholder="0" disabled value={value} />
         </div>
         <div className="calculater_lower_div">
-          <Button Error={setErrer} Value={setValue} />
+          <Button Error={setErrer} Value={result} />
         </div>
       </div>
     </section>
