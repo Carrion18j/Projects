f72119f1d111d0c59098ffaf435ae4134a2ec354
Carrion <Carrion18j@gmail.com>
2023-05-20 12:32:29 +0530

    .

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,14 +2,9 @@ import { Hero, Navigation, About, WhatIDo, Contact } from "./section";
 
 function App() {
   return (
-    <section className="flex flex-col items-center bg-[#1B1F24]">
-      <div className="bg-[#1B1F24]">
-        <Hero />
-        <Navigation />
-        <About />
-        <WhatIDo />
-        <Contact />
-      </div>
+    <section className="flex flex-col items-center ">
+      <Navigation/>
+      <Hero/>
     </section>
   );
 }
--- portfolio/src/section/Hero.jsx
+++ portfolio/src/section/Hero.jsx
@@ -1,22 +1,31 @@
 import React from "react";
-import { heroBg, heroBg2 } from "../pics";
+import "./css/hero.css";
+import bgPattern from "../pics/bgPattern.jpg";
 
 const Hero = () => {
   return (
-    <section className="w-[1800px] aspect-[1.68/1]">
-      <div className="mt-[80px]">
-        <img
-          src={heroBg}
-          alt=""
-          className="w-[1800px] aspect-[1.68/1] brightness-75 absolute left-[50%] top-[0%] translate-x-[-50%]"
-        />
-        <div className="relative z-[1] ml-[100px]">
-          <img
-            src={heroBg2}
-            alt=""
-            className="h-[900px] aspect-[1.2/1.8] z-[10] opacity-80"
-          />
+    <section className="min-h-[90vh] bg-[#222831] w-[100%] flex flex-wrap">
+      <div className="h-[90vh] w-[60%] flex flex-col justify-center ">
+        <div className="ml-[10%] flex flex-col">
+          <h1 className="w-[230px] -mb-[24px] bg-gradient-to-r from-[#e85353] via-[#ff8f8f] to-[#d91010] text-transparent bg-clip-text text-[80px] inline-block font-cursive">
+            HELLO!
+          </h1>
+          <h1 className="text-[46px] text-[#ddcdcd]">I'AM SANIDHYA</h1>
+          <p className="text-[26px] text-white">
+            100% Html Css React Tamplates Made By Sanidhya
+          </p>
+          <button className="hireMeButton font-semibold opacity-[60%] mt-[30px] text-[26px] w-[120px] rounded-[8px] bg-red-100">
+            Hire Me
+          </button>
         </div>
+      </div>
+      <div className="boxShadow" />
+      <div className="h-[100%] w-[40%] border-[#1f1f1f] border-[10px]">
+        <img
+          src={bgPattern}
+          alt="pattern"
+          className="h-[90vh] w-[40vw] opacity-[40%]"
+        />
       </div>
     </section>
   );
