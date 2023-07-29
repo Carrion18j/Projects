f6efc647aab91dc629a7cf18020a6361be9ad5ec
Carrion <125335787+Carrion18j@users.noreply.github.com>
2023-07-29 19:05:19 +0530

    deleted a project

--- portfolio-to-do-webapp/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Button from "./Button";
-import TodoItem from "./TodoItem";
-import FormWindow from "./FormWindow";
-
-
-export { Button, TodoItem, FormWindow };
--- portfolio-to-do-webapp/src/sections/index.js
+++
@@ -1,6 +0,0 @@
-import Home from "./Home";
-import Login from "./Login";
-import Error from "./Error";
-import NotFoundPage from "./NotFoundPage";
-
-export { Home, Login, Error, NotFoundPage };
